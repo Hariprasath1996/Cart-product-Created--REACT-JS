f32a7f821fd910814f6461a7fdf6421a167e5204
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-05 13:04:29 +0530

    created another method

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -56,37 +56,56 @@ import { useState } from "react";
 
 // type : 3
 // entered input values thats get from input element 
+// const User = () => {
+//     const [user, setUser] = useState({ name: "", age: "" })
+
+//     const changeName = (e) => {
+//         setUser((user) => {
+//             return {
+//                 ...user, name: e.target.value
+//             }
+//         })
+//     }
+
+//     const changeAge = (e) => {
+//         setUser((user) => {
+//             return {
+//                 ...user, age: e.target.value
+//             }
+//         })
+//     }
+
+//     return (
+//         <>
+//             <h1>{user.name}</h1>
+//             <h1>{user.age}</h1>
+//             <form >
+//                 <label >Enter a Name :</label> <input type="text" onChange={changeName} />
+//                 <br />
+//                 <label >Enter a Age :</label><input type="number" onChange={changeAge} />
+//             </form>
+
+//         </>
+//     );
+// }
+
+// export default User;
+
 const User = () => {
     const [user, setUser] = useState({ name: "", age: "" })
-
-    const changeName = (e) => {
-        setUser((user) => {
-            return {
-                ...user, name: e.target.value
-            }
-        })
+    const ChangeHandler = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
     }
-
-    const changeAge = (e) => {
-        setUser((user) => {
-            return {
-                ...user, age: e.target.value
-            }
-        })
-    }
-
     return (
         <>
             <h1>{user.name}</h1>
             <h1>{user.age}</h1>
             <form >
-                <label >Enter a Name :</label> <input type="text" onChange={changeName} />
+                <label >Enter a Name :</label><input type="text" name="name" onChange={ChangeHandler} />
                 <br />
-                <label >Enter a Age :</label><input type="number" onChange={changeAge} />
+                <label >Enter a Age :</label><input type="number" name="age" onChange={ChangeHandler} />
             </form>
-
         </>
     );
 }
-
 export default User;
